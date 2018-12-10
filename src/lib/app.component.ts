a4fe1effc6bcce48f47e164d3a6dfe66a6a5c4a5
Duc Anh Trinh <ducanh.trinh@deecoob.com>
2018-12-10 14:57:38 +0100

    fixed gitignore

--- src/lib/app.component.ts
+++ src/lib/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommunicationService } from './shared/communication.service';
 import { QUESTIONS } from './shared/question.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'gl-root',
@@ -8,7 +9,7 @@ import { QUESTIONS } from './shared/question.service';
   styleUrls: ['./app.component.scss']
 })
 export class RootComponent implements OnInit {
-  texts = this.communication.loadData();
+  texts = this.communication.loadData().pipe(map(data => data.slice(0, 5)));
   questions = QUESTIONS;
   constructor(public communication: CommunicationService) {}
 
