ec301673172cb2218b050d5222d0f9f23da556b0
Duc Anh Trinh <ducanh.trinh@deecoob.com>
2018-12-10 17:20:07 +0100

    fixed navbar icon

--- src/lib/navbar/navbar.component.ts
+++ src/lib/navbar/navbar.component.ts
@@ -1,25 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, transition, useAnimation } from '@angular/animations';
-import { pulseAnimation } from '../shared/animations';
+// import { trigger, transition, useAnimation } from '@angular/animations';
+// import { pulseAnimation } from '../shared/animations';
 import { GamificationService } from '../shared/gamification.service';
 
 @Component({
   selector: 'gl-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
-  animations: [
-    trigger('scoreChange', [
-      transition(
-        ':increment',
-        useAnimation(pulseAnimation, {
-          params: {
-            timings: '400ms cubic-bezier(.11,.99,.83,.43)',
-            scale: 1.25
-          }
-        })
-      )
-    ])
-  ]
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
   gamification = this.gamificationService;
