f7960880892d64843efb4fcdabf0b2cd48b89674
Duc Anh Trinh <ducanh.trinh@deecoob.com>
2018-12-12 16:57:04 +0100

    added apollo gql and first dummy auth

--- src/lib/app.component.ts
+++ src/lib/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommunicationService } from './shared/communication.service';
 import { QUESTIONS } from './shared/question.service';
 import { map } from 'rxjs/operators';
+import { UserService } from './shared/user.service';
 
 @Component({
   selector: 'gl-root',
@@ -11,12 +12,19 @@ import { map } from 'rxjs/operators';
 export class RootComponent implements OnInit {
   texts = this.communication.loadData().pipe(map(data => data.slice(0, 5)));
   questions = QUESTIONS;
-  constructor(public communication: CommunicationService) {}
+  constructor(
+    public communication: CommunicationService,
+    private user: UserService
+  ) {}
 
   ngOnInit() {
     // this.communication.loadData().subscribe(res => {
     //   this.texts = res;
     // });
     // this.questions = QUESTIONS;
+    this.user.login(
+      'test',
+      '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
+    );
   }
 }
--- src/lib/shared/user.service.ts
+++ src/lib/shared/user.service.ts
@@ -1,13 +1,65 @@
 import { Injectable } from '@angular/core';
-import { CommunicationService } from './communication.service';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  user;
   userId: string;
+  private token = null;
 
-  constructor(private communication: CommunicationService) {}
+  constructor(private apollo: Apollo) {}
 
-  login(password: string) {}
+  login(username: string, password: string): void {
+    this.apollo
+      .query({
+        query: gql`
+          {
+            login(username: "${username}", password: "${password}") {
+              id,
+              firstName,
+              lastName,
+              username,
+              jwtToken {
+                token
+              }
+            }
+          }
+        `
+      })
+      .subscribe((res: any) => {
+        this.user = res.data.login;
+        // this.userId = user.id;
+        // this.token = user.jwtToken.token;
+        console.log(this.user);
+      });
+  }
+
+  logout(): void {
+    this.apollo.query({
+      query: gql`
+        {
+          logout()
+        }
+      `
+    });
+  }
+
+  islogged(): Observable<boolean> {
+    return this.apollo
+      .query({
+        query: gql`
+          {
+            me {
+              active
+            }
+          }
+        `
+      })
+      .pipe(map((res: any) => res.data.active));
+  }
 }
